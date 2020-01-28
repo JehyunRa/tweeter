f5bb33474b831b1acf93c8619408367f55446442
JehyunRa <jra4862@gmail.com>
2020-01-28 17:04:16 +0000

    03 pulled data from db and displayed on screen

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,3 +8,21 @@ function textAreaAdjust(textArea) {
   textArea.style.height = "0px";
   textArea.style.height = (3 + textArea.scrollHeight)+"px";
 }
+
+function displayTweets(tweets) {
+  for (const tweet of tweets) {
+    console.log(tweet.user.name);
+    $("#display-tweets").append(`<p>${tweet.user.name}</p>`);
+  }
+}
+
+function loadTweets() {
+  console.log('performing ajax call...');
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (result) {
+    console.log('Success: ', result);
+    displayTweets(result);
+  });
+}
+
+loadTweets();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,6 @@
-$(document).ready(function() {
-  console.log('hello');
-})
-
 function textCount(textArea) {
   let n = 140 - textArea.value.length;
   $('#counter').text(n);
   if (n < 0) $('#counter').css("color", "red");
   else $('#counter').css("color", "black");
 }
-
-// function countChar(val) {
-//   var len = val.value.length;
-//   if (len >= 500) {
-//     val.value = val.value.substring(0, 500);
-//   } else {
-//     $('#charNum').text(500 - len);
-//   }
-// }
