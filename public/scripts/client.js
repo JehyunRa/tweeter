2d01b910777b9ffeec1ae2525b644c5c6efd232b
JehyunRa <jra4862@gmail.com>
2020-01-29 19:34:03 +0000

    12 fixed date display in tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,51 @@
 
 $(document).ready(function() {
 
+  function pad2(number) {
+    return (number < 10 ? '0' : '') + number
+  }
+
+  function numberToDate(num) {
+    let seconds = num / 1000;
+    let days = seconds / (60 * 60 * 24);
+    seconds -= Math.floor(days) * (60 * 60 * 24);
+    
+    let year = 1970;
+    let month = 1;
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let hour = 0;
+    let minute = 0;
+    
+    while (days > 365) {
+      if (year % 4 !== 0) {
+        days -= 365;
+        year += 1;
+      } else {
+        days -= 366;
+        year += 1;
+      }
+    }
+    
+    for(let i = 0; i < 12; i++) {
+      if (days >= daysInMonth[i]) {
+        days -= daysInMonth[i];
+        month += 1;
+      } else break;
+    }
+    
+    while (seconds > 60 * 60) {
+      seconds -= 60 * 60;
+      hour += 1;
+    }
+
+    while (seconds > 60) {
+      seconds -= 60;
+      minute += 1;
+    }
+
+    return `${year}/${month}/${Math.floor(days) + 1} ${pad2(hour)}:${pad2(minute)}`;
+  }
+
   function createTweetElements(tweet) {
     const markup = `
         <article class="center">
@@ -19,7 +64,7 @@ $(document).ready(function() {
           </div>
           <hr2/>
           <div>
-            <span class="floatLeft">${tweet.created_at}</span>
+            <span class="floatLeft">${numberToDate(tweet.created_at)}</span>
             <p class="floatRight">features</p>
             <div class="clear"></div>
           </div>
