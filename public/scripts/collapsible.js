b18cd3d17d82d17077ab52da26c8ca1ef96b6ef8
JehyunRa <jra4862@gmail.com>
2020-01-29 17:38:40 +0000

    10 added sliding new tweet textbox

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -1,47 +1,9 @@
 $(document).ready(function() {
 
-  let newTweetDisplay = 1;
+  let newTweetCollapse = 1;
   $(".b").on('click', function() {
-
-      $('.new-tweet').toggleClass('new-tweet-collapse');
-
+    if (newTweetCollapse === 1) $('.new-tweet').slideUp(300, 'swing', () => newTweetCollapse = 0);
+    else $('.new-tweet').slideDown(300, 'swing', () => newTweetCollapse = 1);
   });
-
-  // function calculateCollapsedScale () {
-
-  //   // Whereas the menu as a whole (title plus items) can act as
-  //   // a proxy for the expanded state.
-  //   const expanded = menu.getBoundingClientRect();
-  //   let animation = '';
-  //   let inverseAnimation = '';
-
-  //   for (let step = 0; step <= 100; step++) {
-  //     // Remap the step value to an eased one.
-  //     let easedStep = ease(step / 100);
   
-  //     // Calculate the scale of the element.
-  //     const xScale = x + (1 - x) * easedStep;
-  //     const yScale = y + (1 - y) * easedStep;
-  
-  //     animation += `${step}% {
-  //       transform: scale(${xScale}, ${yScale});
-  //     }`;
-  
-  //     // And now the inverse for the contents.
-  //     const invXScale = 1 / xScale;
-  //     const invYScale = 1 / yScale;
-  //     inverseAnimation += `${step}% {
-  //       transform: scale(${invXScale}, ${invYScale});
-  //     }`;
-  //   }
-
-  //   return `
-  //   @keyframes newTweetAnimation {
-  //     ${animation}
-  //   }
-  
-  //   @keyframes menuContentsAnimation {
-  //     ${inverseAnimation}
-  //   }`;
-  // }
 });
