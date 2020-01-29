bf55b98aabb45e6cba0c63058a2aafd7d9723513
JehyunRa <jra4862@gmail.com>
2020-01-29 22:01:34 +0000

    added responsive design for mobile and desktop

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   let newTweetCollapse = 1;
-  $(".b").on('click', function() {
+  $(".navB").on('click', function() {
     if (newTweetCollapse === 1) $('.new-tweet').slideUp(300, 'swing', () => newTweetCollapse = 0);
     else $('.new-tweet').slideDown(300, 'swing', () => newTweetCollapse = 1);
   });
