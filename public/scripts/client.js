4bcaa5dd7c9df1666165848b1c49c8d721b4a943
JehyunRa <jra4862@gmail.com>
2020-01-29 00:41:32 +0000

    07 completely removed inline functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,43 +6,42 @@
 
 $(document).ready(function() {
 
-});
+  function createTweetElements(tweet) {
+    const markup = `
+        <article class="center">
+          <div class="wrapper">
+            <div class="a"><img src=${tweet.user.avatars}></img></div>
+            <div class="b"><p>${tweet.user.name}</p></div>
+            <div class="c"><p class="floatRight hoverText">${tweet.user.handle}</p></div>
+          </div>
+          <div>
+            <div>${tweet.content.text}</div>
+          </div>
+          <hr2/>
+          <div>
+            <span class="floatLeft">${tweet.created_at}</span>
+            <p class="floatRight">features</p>
+            <div class="clear"></div>
+          </div>
+        </article>
+      `;
+    return markup;
+  };
+  
+  function renderTweets() {
+    console.log('render start');
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweets) {
+      console.log('received data:\n'+tweets)
+      for (const tweet of tweets) {
+        let markup = createTweetElements(tweet);
+        $("#display-tweets").append(markup);
+      }
+    })
+  };
 
-function createTweetElements(tweet) {
-  const markup = `
-      <article class="center">
-        <div class="wrapper">
-          <div class="a"><img src=${tweet.user.avatars}></img></div>
-          <div class="b"><p>${tweet.user.name}</p></div>
-          <div class="c"><p class="floatRight hoverText">${tweet.user.handle}</p></div>
-        </div>
-        <div>
-          <div>${tweet.content.text}</div>
-        </div>
-        <hr2/>
-        <div>
-          <span class="floatLeft">${tweet.created_at}</span>
-          <p class="floatRight">features</p>
-          <div class="clear"></div>
-        </div>
-      </article>
-    `;
-  return markup;
-};
-
-function renderTweets() {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (tweets) {
-    console.log(tweets);
-    for (const tweet of tweets) {
-      let markup = createTweetElements(tweet);
-      $("#display-tweets").append(markup);
-    }
-  })
-};
-
-function postTweet(form) {
-  let data = $(form).serialize();
+  function postTweet(form) {
+    let data = $(form).serialize();
     $.ajax({
       url: '/tweets',
       data,
@@ -50,16 +49,15 @@ function postTweet(form) {
     }).then(() => {
       $("#display-tweets").empty();
       renderTweets();
-    }
-  )};
- 
- function formValidation(form) {
-   event.preventDefault();
-   let check = (document.getElementById("tweetTextArea")).value;
-   if (check === "" || check.length >= 140) alert('unacceptable tweet input');
-   else postTweet(form);
- };
- 
-// Initialization
+    })
+  };
 
-renderTweets();
+  $("#tweetTextForm").on('submit', function(e) {
+    e.preventDefault();
+    let check = (document.getElementById("tweetTextArea")).value;
+    if (check === "" || check.length >= 140) alert('unacceptable tweet input');
+    else postTweet(this);
+  });
+
+  renderTweets();  
+});
