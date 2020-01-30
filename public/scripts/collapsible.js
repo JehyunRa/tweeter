30c7340a798669a18a7e2264a7103eb8c016318a
JehyunRa <jra4862@gmail.com>
2020-01-30 19:01:10 +0000

    22 improved collapsible.js

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
 
   // toggle new-tweet on / off
-  let newTweetCollapse = 0;
   $(".navB").on('click', function() {
-    if (newTweetCollapse === 1) $('.new-tweet').slideUp(300, 'swing', () => newTweetCollapse = 0);
-    else $('.new-tweet').slideDown(300, 'swing', () => newTweetCollapse = 1);
+    $('.new-tweet').slideToggle(300, 'swing');
   });
   
   $('.new-tweet').slideUp(300, 'swing');
