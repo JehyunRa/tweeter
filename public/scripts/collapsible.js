a38d9f0136e787815211626c3ebb1ef2d441c6b4
JehyunRa <jra4862@gmail.com>
2020-01-30 23:06:32 +0000

    26 added .focus() on textbox upon .newTweet toggle

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
 
   // toggle new-tweet on / off
   $(".navB").on('click', function() {
+
     $('.new-tweet').slideToggle(300, 'swing');
+    $('#tweetTextArea').focus();
+
   });
   
   $('.new-tweet').slideUp(300, 'swing');
